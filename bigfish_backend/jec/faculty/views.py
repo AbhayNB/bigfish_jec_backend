46ed6d6224b3ab942774180298d14cf9d212fe70
AbhayNB <101196045+AbhayNB@users.noreply.github.com>
2022-10-22 18:54:26 +0530

    Update views.py

--- bigfish_backend/jec/faculty/views.py
+++ bigfish_backend/jec/faculty/views.py
@@ -6,6 +6,15 @@ from .models import Faculty
 # Create your views here.
 def FacultyData(request):
     fdata=Faculty.objects.all()
-    fname=[a.name for a in fdata]
-    f_name_str="\n".join(fname)
-    return HttpResponse("Our faculty names are:\n>>"+f_name_str)
+    # fname=[a.name for a in fdata]
+    # f_name_str="\n".join(fname)
+    # return HttpResponse("Our faculty names are:\n>>"+f_name_str)
+    Department_query = request.GET.get('Department')
+    Teacher_query = request.GET.get('Teacher')
+    print(Department_query)
+    if Department_query != '' and Department_query is not None:
+        fdata = fdata.filter(department__icontains=Department_query)
+    if Teacher_query != '' and Teacher_query is not None:
+        fdata = fdata.filter(name__icontains=Teacher_query)
+    context={'queryset':fdata}
+    return render(request,'faculty/facultydata.html',context)
