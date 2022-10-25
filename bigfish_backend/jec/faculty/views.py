3d1b4c3b5cb56da1dfef1c27db646b7c30a6d9a4
Abhay Bairagi <101196045+AbhayNB@users.noreply.github.com>
2022-10-25 21:05:14 +0530

    Update views.py

--- bigfish_backend/jec/faculty/views.py
+++ bigfish_backend/jec/faculty/views.py
@@ -11,10 +11,16 @@ def FacultyData(request):
     # return HttpResponse("Our faculty names are:\n>>"+f_name_str)
     Department_query = request.GET.get('Department')
     Teacher_query = request.GET.get('Teacher')
+    desig_query=request.GET.get('designation')
+    regular_query=request.GET.get('regular')
     print(Department_query)
     if Department_query != '' and Department_query is not None:
         fdata = fdata.filter(department__icontains=Department_query)
     if Teacher_query != '' and Teacher_query is not None:
         fdata = fdata.filter(name__icontains=Teacher_query)
+    if (desig_query != 'Choose...' or '') and desig_query is not None:
+        fdata = fdata.filter(designation__icontains=desig_query)
+    if regular_query != '' and regular_query is not None:
+        fdata = fdata.filter(regular=True)
     context={'queryset':fdata}
     return render(request,'faculty/facultydata.html',context)
